6e8b050ead30935e0d9b6cc9e5d207b1e51ae409
digvijay1121 <digvijayrajput1121@gmail.com>
2026-07-08 15:43:01 +0530

    Implement S3 upload functionality

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,1 +1,16 @@
-print("Uploading Image to S3")
+import boto3
+import os
+
+# Create S3 client
+s3 = boto3.client(
+    "s3",
+    region_name=os.environ["AWS_REGION"]
+)
+
+bucket_name = os.environ["S3_BUCKET_NAME"]
+file_name = "employee.jpg"
+
+# Upload image
+s3.upload_file(file_name, bucket_name, file_name)
+
+print(f"{file_name} uploaded successfully to {bucket_name}")
